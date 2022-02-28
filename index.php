d3af1a3d10eaef427a0beb4634e39a97780523f1
YJAPA1993 <91586580+YJAPA1993@users.noreply.github.com>
2022-02-28 17:18:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+        <?php
+            $i = 10;
+            $j = 20;
+            echo "Hello! php";
+            echo "<br/><br/>";
+            echo $i + $j;            
+        ?>
+    </body>
+
+</html>
