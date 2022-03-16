3505be2f0a1eb30ec4db2761c6626130bd086cc8
YJAPA1993 <91586580+YJAPA1993@users.noreply.github.com>
2022-03-17 01:06:57 +0800

    Create summer.php

---
+++ summer.php
@@ -0,0 +1,118 @@
+<html>
+    <head>
+        <title>墾丁三日遊</title>
+        <link rel="icon" href="/img/logo.bmp" type="image/x-icon" />
+        <font size=10 color=e39237><center><B>墾丁三日遊</B></center></font>
+    </head>
+    <html>
+
+    <body background='https://bpic.588ku.com/element_origin_min_pic/17/03/11/e0cdd867885eb0c8b02bbef9c0a78194.jpg'>
+
+
+    <div align="right">
+    <h4><font color="##696969">發起人:<a href="https://www.facebook.com/ihsien.ting" target="new">丁丁丁丁丁丁</a></font></h4>
+    </div>
+
+    <h5><font color="#9c6368">
+    <MARQUEE>
+    雪花隨風飄&nbsp馬路在奔跑&nbsp聖誕帥丁丁&nbsp開著帥氣跑車&nbsp&nbsp&nbsp
+    經過了高大&nbsp渡過了後勁溪&nbsp跟著和平歡喜歌聲翩然地來到&nbsp&nbsp&nbsp
+    叮叮噹&nbsp叮叮噹&nbsp&nbsp鈴聲多響亮&nbsp&nbsp&nbsp
+    你看他呀不避風霜&nbsp&nbsp面容多麼慈祥&nbsp&nbsp&nbsp
+    啦啦啦&nbsp啦啦啦&nbsp&nbsp歌聲多響亮&nbsp&nbsp&nbsp
+    他給我們帶來幸福&nbsp&nbsp大家都歐趴&nbsp嘿
+    </MARQUEE>
+    </font></h5>
+
+    <div align="center">
+        <h3><font color="#3e2928">
+        暑假沒有安排活動嗎?<br/>
+        還是孤單<u>一個人</u>嗎?<br/>
+        想跟丁丁來場浪漫的約會嗎?<br/>
+        <font color="red">那就趕快來報名吧</font>
+        </font></h3>
+    </div>
+
+    <center>
+    <a href="https://www.youtube.com/">
+    <img src="/img/summer-01.jpg" alt="summer" height="300" width="600">
+    </a>
+    <br/>
+    <br/>
+    <a href="https://www.facebook.com/">
+    <img src="/img/images.jfif" alt="summer" height="300" width="600">
+    </a>
+    </center>
+
+    <div align="center">
+    <h1><font color="#3e2928" face="Ink Free">Let's go!!!</font></h1>
+    </div>
+
+    <div align="center">
+    <font size=3>
+    <form action="" method="">
+
+        Name:
+        <input type="text" placeholder="name" require>
+        <br/>
+
+        Email:
+        <input type="email">
+        <br/>
+
+        tel:
+        <input type="tel">
+        <br/>
+
+        Gender:
+        <input type="radio" name="gender">M
+        <input type="radio" name="gender">F
+        <input type="radio" name="gender">serect
+        <br/>
+
+        Food Preference:
+        <input type="checkbox" checked>無肉不歡
+        <input type="checkbox">吃肉太殘忍
+        <input type="checkbox">仙女只喝水
+        <br/>
+
+        T-shirt size:
+        <select>
+            <option>S
+            <option selected>M
+            <option>L
+            <option>XL
+        </select>
+        <br/>
+
+        Birthday:
+        <input type="date">
+        <br/>
+
+        Tickets:
+        <input type="number">
+        <br/>
+        <br/>
+        <br/>
+
+        <input type="submit" value="Submit"><input type="reset" value="Reset">
+    </form>
+    </font>
+    </div>
+
+
+    <center>
+        <font size=4>T-shirt size尺寸對照表:</font>
+        </br>
+        </br>
+        <table border="1">
+            <tr><td>\</td><td>S</td><td>M</td><td>L</td><td>XL</td></tr>
+            <tr><td>長</td><td>100</td><td>150</td><td>180</td><td>200</td></tr>
+            <tr><td>寬</td><td>100</td><td>150</td><td>180</td><td>200</td></tr>
+            <tr><td>高</td><td>100</td><td>150</td><td>180</td><td>200</td></tr>
+        </table>
+    </center>
+
+    </body>
+
+</html>
